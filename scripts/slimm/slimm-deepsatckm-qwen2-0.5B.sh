073004df599ad572788c0b8cc46a48f4c36cda16
lingchen meng <lcmeng20@fudan.edu.cn>
2024-12-26 20:32:06 +0800

    update training scripts

--- scripts/slimm/slimm-deepsatckm-qwen2-0.5B.sh
+++ scripts/slimm/slimm-deepsatckm-qwen2-0.5B.sh
@@ -1,3 +1,5 @@
+# we recommend 8 GPUs for each job
+
 DATA_PATH=scripts/data/lcs_558k_pt.yaml
 MODEL_NAME=SliMM-DeepStackM-Qwen2-0.5B/stage1
 
@@ -18,7 +20,7 @@ DATA_PATH=scripts/data/llava_ov_mid.yaml
 MODEL_NAME=SliMM-DeepStackM-Qwen2-0.5B/stage1.5
 CKPT=checkpoints/$MODEL_NAME
 
-bash jobs/qwen2_vl/qwen2vl_template.sh $MODEL_NAME $DATA_PATH  \
+bash scripts/slimm/job_template.sh $MODEL_NAME $DATA_PATH  \
     --model_name_or_path $PREV_CKPT \
     --per_device_train_batch_size 32 --gradient_accumulation_steps 1 --max_num_vistoken 1024 \
     --model_max_length 4096 \
@@ -34,7 +36,7 @@ DATA_PATH=scripts/data/llava_ov_si.yaml
 MODEL_NAME=SliMM-DeepStackM-Qwen2-0.5B/
 CKPT=checkpoints/$MODEL_NAME
 
-bash jobs/qwen2_vl/qwen2vl_template.sh $MODEL_NAME $DATA_PATH  \
+bash scripts/slimm/job_template.sh $MODEL_NAME $DATA_PATH  \
     --model_name_or_path $PREV_CKPT \
     --per_device_train_batch_size 8 --gradient_accumulation_steps 2 --max_num_vistoken 1024 \
     --model_max_length 4096 \
